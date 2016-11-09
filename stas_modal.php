766625b8253467e7107455c99cd6d5127636c0b4
pk1z <pk1z@github.com>
2016-11-09 16:22:55 +0300

    исправил название класса

--- stas_modal.php
+++ stas_modal.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die('Restricted access');
 
 $doc = JFactory::getDocument();
 $doc->addStyleSheet ("http://xn--h1aaljkbdi.xn--p1ai/promo/css/stas.css");
-$doc->addScript(JURI::root().'plugins/stas_modal/stas_modal.js');
+$doc->addScript(JUri::root().'plugins/stas_modal/stas_modal.js');
 
 ?>
 
