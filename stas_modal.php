01c2908b196df6170ab3e237ab8c6bb15f8ef2c2
stas <stasro@bk.ru>
2016-11-11 19:24:49 +0300

    добавил код из stas_modal.php в stasmodal.php

--- stas_modal.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('_JEXEC') or die('Restricted access');
-
-$doc = JFactory::getDocument();
-$doc->addStyleSheet("http://xn--h1aaljkbdi.xn--p1ai/promo/css/stas.css");
-$doc->addScript(JUri::root() . 'plugins/content/stas_modal/jquery-3.1.1.min.js');
-$doc->addScript(JUri::root() . 'plugins/content/stas_modal/stas_modal.js');
-
