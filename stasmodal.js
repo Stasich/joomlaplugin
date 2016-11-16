eeee5157bedea400b813ae79b27e1cca2afb9c3f
stas <stasro@bk.ru>
2016-11-16 11:20:41 +0300

    Заменил $ на jQuery. Сменил событие, запускающее отправку формы .

--- stasmodal.js
+++ stasmodal.js
@@ -1,22 +1,22 @@
 /**
  * Created by stas on 09.11.2016.
  */
-$(document).ready(function () {
-    $('input[name=addtocart]').attr('type','button');
-    $('span.addtocart-button').click(function () {
-        $('#overlay_stas').fadeIn(400,
+jQuery(document).ready(function () {
+    jQuery('input[name=addtocart]').attr('type','button');
+    jQuery('span.addtocart-button').click(function () {
+        jQuery('#overlay_stas').fadeIn(400,
             function () {
-                $('#modal_stas')
+                jQuery('#modal_stas')
                     .css('display', 'block')
                     .animate({opacity: 1, top: '50%'}, 200);
             }
         );
     });
 
-    $("<div id='modal_stas'> " +
+    jQuery("<div id='modal_stas'> " +
         "<span id='modal_close_stas'>X</span>" +
         "<div><p class='stasformtext_stasmodal'>Заполните форму, что бы узнать актуальные цены!</p></div>"+
-        "<form class='form-horizontal' method='post' action='/plugins/content/stasmodal/mail.php'>" +
+        "<form id='stas_modal_form' class='form-horizontal' method='post' action='javascript:0'>" +
             "<div class='form-groups_stasmodal'>" +
                 "<label class='labelstas_stasmodal' for='name'>Имя:</label>" +
                 "<div><input id='name_stasmodal' name='name' required='' type='text' placeholder='Введите Имя*' /></div>" +
@@ -36,17 +36,17 @@ $(document).ready(function () {
       "</div>" +
     "<div id='overlay_stas'></div>").insertAfter("#main");
 
-    $('#modal_close_stas, #overlay_stas').click(function () {
-        $('#modal_stas, #overlay_stas').css('display', 'none');
+    jQuery('#modal_close_stas, #overlay_stas').click(function () {
+        jQuery('#modal_stas, #overlay_stas').css('display', 'none');
     });
-    $('#stas_button').click(function () {
-        $.post('/plugins/content/stasmodal/mail.php', {
-            name: $('#name_stasmodal').val(),
-            email: $('#email_stasmodal').val(),
-            mess: $('#textarea_stasmodal').val()
+    jQuery('#stas_modal_form').submit(function () {
+        jQuery.post('/plugins/content/stasmodal/mail.php', {
+            name: jQuery('#name_stasmodal').val(),
+            email: jQuery('#email_stasmodal').val(),
+            mess: jQuery('#textarea_stasmodal').val()
         })
             .done(function() {
-                $('#modal_stas, #overlay_stas').css('display', 'none');
+                jQuery('#modal_stas, #overlay_stas').css('display', 'none');
                 alert( "Ваша заявка отправленна! В течении дня менеджер ответит Вам на указанную почту." );
             })
             .fail(function(){
